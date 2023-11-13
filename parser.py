cc9f9bd12a03890d0eb516b5d34dbd6d00f2cc0c
InfSub <inf.sub+github.com@gmail.com>
2023-11-14 00:10:18 +0300

    ver. 1.1.0: Refactored

--- parser.py
+++ parser.py
@@ -22,21 +22,21 @@ class Bcolors:
 
 
 class Parser:
-    def __init__(self, url: str = None, deep: int = 1) -> None:
-        self.__urls = None
-        self.__deep = None
+    def __init__(self, url: str = None, deep: int = None) -> None:
+        self._urls = None
+        self._deep = None
 
-        self.__cur_url = None
-        self.__cur_parse_urls = None
-        self.__cur_deep = None
-        self.__cur_domain = None
-        self.__cur_check_url = None
-        self.__https = 'https://'
+        self._cur_url = None
+        self._cur_parse_urls = None
+        self._cur_deep = None
+        self._cur_domain = None
+        self._cur_check_url = None
+        self._https = 'https://'
 
-        self.__count = 0
+        self._count = 0
 
-        self.__save_file = 'result.json'
-        self.__re_domain = \
+        self._save_file = 'result.json'
+        self._re_domain = \
             r'https?:\/\/((?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9])\/.*'
 
         self.set_url(url=url)
@@ -45,55 +45,55 @@ class Parser:
     def set_url(self, url: str) -> None:
         urls = {}
         if url is not None:
-            url = url if self.__check_domain(url) else f'{self.__https}{url}'
+            url = url if self._check_domain(url) else f'{self._https}{url}'
             urls.update({url: {}})
-            self.__urls = urls
+            self._urls = urls
 
     def set_deep(self, deep: int = 1) -> None:
-        self.__deep = deep
+        self._deep = deep
 
     def print_result(self) -> None:
-        print(json.dumps(self.__urls, indent=4, ensure_ascii=False, sort_keys=True))
+        print(json.dumps(self._urls, indent=4, ensure_ascii=False, sort_keys=True))
 
     def save_result(self, file: str = None) -> None:
         if file is None:
-            file = self.__save_file
+            file = self._save_file
         with open(file, 'w') as json_file:
-            json.dump(self.__urls, json_file, indent=4, ensure_ascii=False, sort_keys=True)
+            json.dump(self._urls, json_file, indent=4, ensure_ascii=False, sort_keys=True)
 
-    def __get_url(self, urls: dict, deep: int = 0) -> None:
-        if deep < self.__deep:
+    def _get_url(self, urls: dict, deep: int = 0) -> None:
+        if deep < self._deep:
             deep += 1
 
             for url in urls:
-                self.__count += 1
-                print(f'count: {self.__count}\t| deep: {deep}\t| url: {url}')
+                self._count += 1
+                print(f'count: {self._count}\t| deep: {deep}\t| url: {url}')
 
-                self.__get_urls_from_page(url)
-                urls[url] = self.__cur_parse_urls
+                self._get_urls_from_page(url)
+                urls[url] = self._cur_parse_urls
 
                 if urls[url]:
-                    self.__get_url(urls=urls[url], deep=deep)
+                    self._get_url(urls=urls[url], deep=deep)
 
-    def __check_domain(self, url: str) -> str:
-        match = re.fullmatch(self.__re_domain, url)
+    def _check_domain(self, url: str) -> str:
+        match = re.fullmatch(self._re_domain, url)
         return match[1] if match and match[1] else ''
 
-    def __get_domain_from_url(self, url: str) -> bool:
-        self.__cur_domain = self.__check_domain(url)
-        return True if self.__cur_domain else False
+    def _get_domain_from_url(self, url: str) -> bool:
+        self._cur_domain = self._check_domain(url)
+        return True if self._cur_domain else False
 
-    def __check_external_url(self, url: str) -> bool:
-        domain = self.__check_domain(url)
-        return True if domain and domain != self.__cur_domain else False
+    def _check_external_url(self, url: str) -> bool:
+        domain = self._check_domain(url)
+        return True if domain and domain != self._cur_domain else False
 
-    def __get_urls_from_page(self, url: str) -> None:
+    def _get_urls_from_page(self, url: str) -> None:
         urls = {}
         ua = UserAgent(browsers=['edge', 'firefox', 'chrome'])
 
         headers = {'User-Agent': ua.chrome}
 
-        if not self.__get_domain_from_url(url):
+        if not self._get_domain_from_url(url):
             print(f'{Bcolors.WARNING}Warning: Domain URL "{url}" is invalid.{Bcolors.ENDC}')
             return
 
@@ -112,13 +112,13 @@ class Parser:
 
         for link in soup.find_all('a'):
             if link.get('href') is not None:
-                if self.__check_external_url(link.get('href')):
+                if self._check_external_url(link.get('href')):
                     urls.update({link.get('href'): {}})
 
-        self.__cur_parse_urls = urls
+        self._cur_parse_urls = urls
 
     def run(self):
-        self.__get_url(urls=self.__urls, deep=0)
+        self._get_url(urls=self._urls, deep=0)
 
 
 if __name__ == '__main__':
